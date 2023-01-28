894b9a89f2507260022db3a672571a336d56451a
AdrineUwera <a.uwera@alustudent.com>
2023-01-28 21:42:30 +0200

    merging with main

--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,5 +1,7 @@
+// imports the material library to use Material widgets
 import 'package:flutter/material.dart';
 
+// a class for a task
 class Todo {
   String title;
   dynamic isCompleted;
@@ -8,90 +10,108 @@ class Todo {
   Todo(this.title, this.isCompleted, this.description, this.priority);
 }
 
+// AllTasksPage is a stateful widget that displays a list of tasks.
 class AllTasksPage extends StatefulWidget {
   @override
   _AllTasksPage createState() => _AllTasksPage();
 }
 
+// _AllTasksPage is the state object for the AllTasksPage widget.
 class _AllTasksPage extends State<AllTasksPage> {
-  // List<String> todos = [ 'Take out the trash' 'Buy groceries' 'Do laundry' ];
+  // A list of Todo objects to be displayed.
   List<Todo> todos = [
-    Todo('Take out the trash', false, 'I will take out the trash', 4),
-    Todo('Buy groceries', false, 'I will but groceries', 2),
-    Todo('Do laundry', true, 'I will do the laundry', 1),
+    Todo('Take out the trash', false, 'The trash bag is full.', 4),
+    Todo('Buy groceries', false, 'I have to buy vegetables and fruits.', 2),
+    Todo('Do laundry', true, 'The laundry basket is full.', 1),
   ];
+
   bool? tapped;
 
   @override
   Widget build(BuildContext context) {
+    // The widget returns a container that wraps the screen content.
     return Container(
-      color: Colors.grey[250],
+      color: Color.fromARGB(255, 248, 248, 248),
+      // centers everything inside the container
       child: Center(
-        child: Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: ListView.builder(
-                itemCount: todos.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Card(
-                          // margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                          elevation: 2,
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: ListTile(
-                            autofocus: false,
-                            leading: Text(
-                              todos[index].priority.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  color: Color(0xFF476EBE),
-                                  fontSize: 25.0,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            title: Text(
-                              todos[index].title,
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            subtitle: Text(
-                              todos[index].description,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 11.0,
-                                  fontWeight: FontWeight.w200),
-                            ),
-                            trailing: Checkbox(
-                              value: todos[index].isCompleted,
-                              onChanged: (bool? value) {
-                                setState(() {
-                                  todos[index].isCompleted = value;
-                                });
-                              },
-                            ),
-                            selected: true,
-                            onTap: () {
-                              setState(() {
-                                tapped = true;
-                              });
-                            },
-                          )),
-                      SizedBox(
-                        height: 15,
-                      )
-                    ],
-                  );
-                },
-              ),
-            )), //Padding
+        // a container wrapper for the list of tasks
+        child: Container(
+          // add margin so that their is space between the screen edges and the tasks list
+          margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          //display the tasks i.e. Todo objects in the list.
+          child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (BuildContext context, int index) {
+              // return a column that streches and occupy the space available cross axis (horizontally)
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  // each task is a card and the card wraps the list tile for each task
+                  Card(
+                      // adds a shadow to the card
+                      elevation: 2,
+                      color: Colors.white,
+                      // makes the cards edges to be round
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      // displays a task. 
+                      child: ListTile(
+                        autofocus: false,
+                        // the widget displayed before the title of the task
+                        leading: Text(
+                          // converts the number to string
+                          todos[index].priority.toString(),
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color(0xFF476EBE),
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        // displays the title of the task
+                        title: Text(
+                          todos[index].title,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        // displays the description of the task
+                        subtitle: Text(
+                          todos[index].description,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 11.0,
+                              fontWeight: FontWeight.w200),
+                        ),
+                        // a widget to be displayed after the title and subtitle
+                        trailing: Checkbox(
+                          // a checkbox that shows if a task is completed or not
+                          value: todos[index].isCompleted,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              todos[index].isCompleted = value;
+                            });
+                          },
+                        ), 
+                        // a function that defines what happens when the list tile is tapped
+                        // for now since nothing happens. we only sets the variable tapped to be true
+                        onTap: () {
+                          setState(() {
+                            tapped = true;
+                          });
+                        },
+                      )),
+                  // a size box that adds space after each task on the list
+                  SizedBox(
+                    height: 15,
+                  )
+                ],
+              );
+            },
+          ),
+        ),
       ), //Center
     ); //SizedBox
   }
--- lib/screens/create_task.dart
+++ lib/screens/create_task.dart
@@ -1,7 +1,10 @@
+// imports the material library to use Material widgets
 import 'package:flutter/material.dart';
 
+// an enum for the status of completeness of a task
 enum Status { completed, not_completed }
 
+// CreateTaskPage is a stateful widget that holds a page where the user can create a task
 class CreateTaskPage extends StatefulWidget {
   const CreateTaskPage({super.key});
 
@@ -9,53 +12,70 @@ class CreateTaskPage extends StatefulWidget {
   State<CreateTaskPage> createState() => _CreateTaskPage();
 }
 
+// _CreateTaskPage is the state object for the CreateTaskPage widget.
 class _CreateTaskPage extends State<CreateTaskPage> {
+  // instance of the Status enum
   Status? task_status;
+  //a variable that will hold the priority that the user gives a task
   var priority = 0.0;
+
+  // overrides the build function of a state object
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Container(
-          margin: const EdgeInsets.all(20),
+      body: Container(
+          // adds margin to create space between the screen edges and the content
+          margin: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Text(
+              // add the title on the page
+              Text(
                 "Add New Task",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-              const TextField(
+              // adds space between the title and the following text field
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // a text field that receives user input for the title of the task
+              TextField(
                 decoration: InputDecoration(
                     labelText: "Title", border: OutlineInputBorder()),
               ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-              const TextField(
+              // adds space between the text fields
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // a text field that receives user input for the description of the task
+              TextField(
                 decoration: InputDecoration(
                     labelText: "Description", border: OutlineInputBorder()),
               ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // adds space after the text field
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+              // a column that holds the task status options
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
+                  // a label for the task status options
+                  Text(
                     "Task status",
                     style: TextStyle(fontSize: 16),
                   ),
+                  // a row for a task status option
                   Row(
                     children: [
+                      // radio button for completed status option
                       Radio(
                         value: Status.completed,
                         groupValue: task_status,
                         onChanged: (val) {
                           setState(() {
+                            // sets the value (task_status enum) of the status completeness to completed
                             task_status = val;
                           });
                         },
                       ),
-                      const Text(
+                      // status option label
+                      Text(
                         "Completed",
                         style: TextStyle(
                           fontSize: 16,
@@ -63,18 +83,22 @@ class _CreateTaskPage extends State<CreateTaskPage> {
                       )
                     ],
                   ),
+                  // a row for a task status option
                   Row(
                     children: [
+                      // radio button for completed status option
                       Radio(
                         value: Status.not_completed,
                         groupValue: task_status,
                         onChanged: (val) {
+                          // sets the value (task_status enum) of the status completeness to not_completed
                           setState(() {
                             task_status = val;
                           });
                         },
                       ),
-                      const Text(
+                      // status option label
+                      Text(
                         "Not Completed",
                         style: TextStyle(
                           fontSize: 16,
@@ -84,15 +108,21 @@ class _CreateTaskPage extends State<CreateTaskPage> {
                   ),
                 ],
               ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // adds a space after before the following column
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+              // a column that holds the task priority slider
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const Text(
+                  // a label for the slider
+                  Text(
                     "Task priority",
                     style: TextStyle(fontSize: 16),
                   ),
+
+                  // a slider that will be used to specify the task priority
                   Slider(
                     value: priority,
                     onChanged: ((value) {
@@ -109,7 +139,9 @@ class _CreateTaskPage extends State<CreateTaskPage> {
                   ),
                 ],
               ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // adds space before the button
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              // a button that is elevated i.e. has a shadow
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
@@ -119,6 +151,6 @@ class _CreateTaskPage extends State<CreateTaskPage> {
               )
             ],
           )),
-    ));
+    );
   }
 }
