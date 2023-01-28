ad5f4d2c8e9589eb1cf8d532c6b0790d086de32e
AdrineUwera <a.uwera@alustudent.com>
2023-01-28 22:17:38 +0200

    Merging everything

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,33 +1,48 @@
-import 'package:flutter/material.dart';
-import 'package:todo_app_mobile/widgets/homeWidget.dart';
+import 'package:flutter/material.dart';  // importing Material library from Flutter
+//importing several dart files that are needed for the application
 import 'package:todo_app_mobile/widgets/my_header_drawer.dart';
 import 'all_tasks.dart';
 import 'create_task.dart';
-
+ 
+// creating a stateful widget for the home screen of the app
 class HomeScreen extends StatefulWidget {
-  // const HomePage({super.key});
-
+ 
+  //creating an instance of the _HomeScreenState class to maintain state and rebuild the widget when necessary
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
-
+ 
+// defining a class used to rebuild the HomeScreen widget when the state changes
 class _HomeScreenState extends State<HomeScreen> {
+ 
+  //defining variable used to determine which page to show in the body of the Scaffold widget, 
+  //and initializing it to the landing page.
   var currentPage = DrawerSections.tasks;
+ 
+  //defining build method
   @override
   Widget build(BuildContext context) {
+    //variable that holds content of a current page being displayed
     var container;
+ 
+    //Defining what page to display according to the option selected in the side menu(drawer)
     if (currentPage == DrawerSections.tasks) {
       container = AllTasksPage();
     } else {
       container = CreateTaskPage();
     }
+ 
+    // returning scaffold widget that defines the layout of the page
     return Scaffold(
-      backgroundColor: Colors.white,
-      floatingActionButton: currentPage == DrawerSections.new_task
+      backgroundColor: Colors.white, // background color of the app page
+      floatingActionButton: currentPage == DrawerSections.new_task  
+          // checks if the page is create task page and not display the floatActionButton
           ? null
+          //otherwise it will display the floatActionButton
           : FloatingActionButton(
               onPressed: () {
                 setState(() {
+                  //navigates to the create task page
                   currentPage = DrawerSections.new_task;
                 });
               },
@@ -35,80 +50,75 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Icon(Icons.add),
             ),
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "ToDo",
-          style: TextStyle(color: Color(0xFF476EBE)),
-        ),
-        centerTitle: true,
-        elevation: 0,
+        backgroundColor: Colors.white,  // defining background color of the app bar
+        title: Text("ToDo",style: TextStyle(color: Color(0xFF476EBE)),), // title in the app bar
+        centerTitle: true, // centering the text in the app bar
+        elevation: 0,      // removing the shadow from the app bar
         actions: [
+          // a popup menu in the app bar
           PopupMenuButton(
               itemBuilder: (context) => [
+                // list on menu items each with an icon and label
                     PopupMenuItem(
                         child: Row(
                       children: [
-                        Icon(Icons.person),
-                        // Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
+                        Icon(Icons.person), 
                         PopupMenuItem(child: Text("Profile")),
                       ],
                     )),
                     PopupMenuItem(
                         child: Row(
                       children: [
-                        Icon(Icons.dark_mode),
-                        // Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
+                        Icon(Icons.dark_mode), 
                         PopupMenuItem(child: Text("Dark mode")),
                       ],
                     )),
                     PopupMenuItem(
                         child: Row(
                       children: [
-                        Icon(Icons.settings),
-                        // Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
+                        Icon(Icons.settings), 
                         PopupMenuItem(child: Text("Settings")),
                       ],
                     )),
                     PopupMenuItem(
                         child: Row(
                       children: [
-                        Icon(Icons.logout),
-                        // Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
+                        Icon(Icons.logout), 
                         PopupMenuItem(child: Text("Logout")),
                       ],
                     )),
                   ])
         ],
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     setState(() { 
-      //       currentPage = DrawerSections.new_task;
-      //     });
-      //   },
-      //   backgroundColor: Color(0xFF476EBE),
-      //   child: Icon(Icons.add),
-      // ),
-      body: container,
+      body: container, // initializing the app body with the content of the current page being displayed
+ 
+      //defining the app drawer widget
       drawer: Drawer(
-          child: SingleChildScrollView(
-        child: Container(
-          child: Column(children: [
-            MyHeaderDrawer(),
-            MyDrawerList(),
+          child: SingleChildScrollView( // a single child scrollable widget that can scroll only vertically
+        child: Container( // container widget that holds content of side panel
+          child: Column(children: [  // content of the container is organised in a column widget
+            MyHeaderDrawer(),    // custom widget that holds the header of app drawer
+            MyDrawerList(),      // custom widget that holds drawer's list of options 
           ]),
         ),
       )),
     );
   }
-
+ 
+// defining custom widget that holds drawer's list of options
   Widget MyDrawerList() {
+ 
+    // container that holds drawer menu items in a column
     return Container(
+      // giving the container a top padding
       padding: EdgeInsets.only(
         top: 15,
-      ),
-      child: Column(
+      ), 
+      child: Column(  // widget that holds drawer menu items in a column
+        // defining drawer menu items
         children: [
+          // calling the menuItem class and passing to it the id, name, and icon of the drawer menu item and a boolean 
+          // specifying whether the menuItem is the current page 
           menuItem(1, "All Tasks", Icons.checklist,
               currentPage == DrawerSections.tasks ? true : false),
           menuItem(2, "Create Task", Icons.add_task,
@@ -117,13 +127,20 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
+ 
+// defining custom widget that creates the drawer menu item
   Widget menuItem(int id, String title, IconData icon, bool selected) {
+ 
+    // returning a material widget to allow material design interactions like tapping on a menu item
     return Material(
-        color: selected ? Colors.grey[300] : Colors.transparent,
+      // defining background color for when menu item is selected and otherwise
+        color: selected ? Colors.grey[300] : Colors.transparent, 
+        // inkwell widget to be able to click on a menu item
         child: InkWell(
           onTap: () {
+            //When tapped, it pops the current context
             Navigator.pop(context);
+            //It also sets the state of the currentPage variable to the corresponding page based on the id passed in 
             setState(() {
               if (id == 1) {
                 currentPage = DrawerSections.tasks;
@@ -132,23 +149,28 @@ class _HomeScreenState extends State<HomeScreen> {
               }
             });
           },
+ 
+          // giving menu item a top,left,bottom,and right padding
           child: Padding(
               padding: EdgeInsets.all(15.0),
+              // organising menu items in a row
               child: Row(
-                // mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  // expanded widget that contains icon of the menu item
                   Expanded(
                     child: Icon(
-                      icon,
-                      size: 20,
-                      color: Colors.black,
+                      icon, // icon passed to the menuItem class
+                      size: 20, // defining size
+                      color: Colors.black, // defining the color of icon
                     ),
                   ),
+ 
+                  // expanded widget that contains text of the menu item
                   Expanded(
-                      flex: 3,
+                      flex: 3, // defining space took up by text in the row
                       child: Text(
-                        title,
-                        style: TextStyle(
+                        title,  // title passed to the menuItem class
+                        style: TextStyle(  // defining text style
                           color: Colors.black,
                           fontSize: 16,
                         ),
@@ -158,7 +180,9 @@ class _HomeScreenState extends State<HomeScreen> {
         ));
   }
 }
-
+ 
+ 
+// enum that holds drawer menu items which will be used to track the displayed page according to the selected menu Item
 enum DrawerSections {
   tasks,
   new_task,
--- lib/widgets/my_header_drawer.dart
+++ lib/widgets/my_header_drawer.dart
@@ -1,38 +1,45 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart'; // importing Material library from Flutter
+ 
+// creating a stateful widget for the app drawer header
 class MyHeaderDrawer extends StatefulWidget {
+  //creating an instance of the _MyHeaderDrawerState class to maintain state and rebuild the widget when necessary
   @override
   _MyHeaderDrawerState createState() => _MyHeaderDrawerState();
 }
-
+ 
+// defining a class used to rebuild the drawer header widget when the state changes
 class _MyHeaderDrawerState extends State<MyHeaderDrawer> {
+ 
+  //defining build method
   @override
   Widget build(BuildContext context) {
+ 
+    // container widget that holds items of drawer header
     return Container(
-      color: Color(0xFF476EBE),
-      width: double.infinity,
-      height: 200,
-      padding: EdgeInsets.only(top: 20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      color: Color(0xFF476EBE),  // background header
+      width: double.infinity,      // defining width
+      height: 200,                 // defining height
+      padding: EdgeInsets.only(top: 20.0), // defining top padding of the header
+      child: Column( // organizing header items in a column
+        mainAxisAlignment: MainAxisAlignment.center, // aligning items at the center of the column
         children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 10),
-            height: 70,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage('assets/images/profile.jpg'),
+          Container(  //container widget to hold user image
+            margin: EdgeInsets.only(bottom: 10), // bottom margin
+            height: 70, // defining container height
+            decoration: BoxDecoration( // widget for styling the container
+              shape: BoxShape.circle,  // making the container a circle
+              image: DecorationImage(  // widget to allow us filling the circle with an image
+                image: AssetImage('assets/images/profile.jpg'), // specifying image path
               ),
             ),
           ),
-          Text(
+          Text( // child of column widget holding user name
             "John Doe",
-            style: TextStyle(color: Colors.white, fontSize: 20),
+            style: TextStyle(color: Colors.white, fontSize: 20), // styling user name
           ),
-          Text(
+          Text( // child of column widget holding user email
             "johndoe@gmail.com",
-            style: TextStyle(
+            style: TextStyle(  // styling user email
               color: Colors.grey[200],
               fontSize: 14,
             ),
